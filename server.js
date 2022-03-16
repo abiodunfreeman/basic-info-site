3760acb7bd05b2dc539528422e24002c17b111ed
Odun <bornduringwar@gmail.com>
2022-03-16 17:52:38 -0400

    server now sends data from index.html to web page

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const http = require('http')
-
+const http = require('http');
+const fs = require('fs');
 const port = 8080;
 
 const server = http.createServer((req, res) => {
     console.log('request made')
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello, world </h1>')
+    
+    fs.readFile('./pages/index.html', 'utf8',  (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        res.write(data);
+        res.end()
+        
+    })
+
 })
 
 server.listen(port, 'localhost', () => {
